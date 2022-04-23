f43f03f2fcee33525d71f461dfbb968f640695ff
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-04-23 21:45:30 +0100

    modifiers: public, private and protected class properties

--- classes.ts
+++ classes.ts
@@ -1,23 +1,27 @@
-// CLASSES IS A BLUEPRINT FOR CREATING AN OBJECT WITH VALUES AND METHODS TO REPRESENT A THING
+// CLASS IS A BLUEPRINT FOR CREATING AN OBJECT WITH VALUES AND METHODS TO REPRESENT A THING
+//MODIFIERS: PUBLIC, PRIVATE, PROTECTED
 
 class MyVehicles {
-  drive(): void {
-    console.log('moving');
-  }
-  hunk(): void{
+  // public drive(): void {
+  //   console.log('start engine');
+  // }
+  protected hunk(): void{
     console.log('pum pum');
     
   }
 }
 
 class Car extends MyVehicles {
-  drive(): void {
+  private drive(): void {
     console.log('vroommmm');
-    
+  }
+  startDriving(): void {
+    this.drive();
+    console.log('speeding');
+    this.hunk();
   }
 }
 
 const car = new Car();
 
-car.drive();
-car.hunk();
+car.startDriving();
